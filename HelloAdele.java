d0c83ea754664409493a71c9b874b8cf7e2013be
SonnyLiuu <sonnyliu0213@gmail.com>
2023-02-19 15:54:39 -0500

    Adele's voice is inbued in a mere print statement

--- HelloAdele.java
+++ HelloAdele.java
@@ -2,6 +2,6 @@ public class HelloAdele
 {
     public static void main(String [] args)
     {
-        System.out.println("Hello, its me.");
+        System.out.println("Hello...its me.");
     }
 }
