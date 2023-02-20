a74e92d2528d2498eb94a6eecff7d4241006347e
SonnyLiuu <sonnyliu0213@gmail.com>
2023-02-19 23:28:33 -0500

    added next verse

--- HelloAdele.java
+++ HelloAdele.java
@@ -3,5 +3,8 @@ public class HelloAdele
     public static void main(String [] args)
     {
         System.out.println("Hello...its me.");
+        System.out.println("I've been wondering
+                            if after all these years
+                            you'd like to meet");
     }
 }
